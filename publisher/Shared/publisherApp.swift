cfc70dc093a6a7d8dd679593946d4d05ff25e468
Victor Baleeiro <me@victorbaleeiro.com.br>
2021-04-05 12:16:18 -0300

    config: initial commit

---
+++ publisher/Shared/publisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  publisherApp.swift
+//  Shared
+//
+//  Created by ACT on 05/04/21.
+//
+
+import SwiftUI
+
+@main
+struct publisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
